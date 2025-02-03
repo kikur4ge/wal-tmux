cff72eb0abf27646fbda713fe5274482b9b35b2c
Aaron <81300255+kikur4ge@users.noreply.github.com>
2025-02-03 08:59:16 +0000

    moved apple music scripts into one script

--- scripts/apple-music.sh
+++ scripts/apple-music.sh
@@ -5,13 +5,36 @@ export LC_ALL=en_US.UTF-8
 current_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $current_dir/utils.sh
 
+function current_track {
+osascript <<EOD
+  if application "Music" is running then
+          tell application "Music"
+                  if player state is playing then
+                          return artist of current track & " - " & name of current track
+                  end if
+          end tell
+  end if
+EOD
+}
+current_track
+
+function echo_track {
+  if [ `uname` = "Darwin" ]; then
+    track="$(current_track)"
+    if [ "$track" ]; then
+      echo "🎵 $track"
+    fi
+  fi
+}
+echo_track
+
+
 main()
 {
   # storing the refresh rate in the variable RATE, default is 5
   RATE=$(get_tmux_option "@dracula-refresh-rate" 5)
 
-  spotify_playback=$(zsh $current_dir/nowplaying.sh)
-  echo "$spotify_playback"
+  echo_track
 }
 
 # run the main driver
--- scripts/current-track.sh
+++
@@ -1,7 +0,0 @@
-if application "Music" is running then
-        tell application "Music"
-                if player state is playing then
-                        return artist of current track & " - " & name of current track
-                end if
-        end tell
-end if
